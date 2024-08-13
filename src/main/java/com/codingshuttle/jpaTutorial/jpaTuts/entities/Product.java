e994fc4c000afaabce1ccf9cb50234ee614bf6bb
Zaid <zaid041999@gmail.com>
2024-08-13 11:02:13 +0530

    Hibernate ORM ,JPA,Entities and Table

---
+++ src/main/java/com/codingshuttle/jpaTutorial/jpaTuts/entities/Product.java
@@ -0,0 +1,48 @@
+package com.codingshuttle.jpaTutorial.jpaTuts.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(
+        name = "product_table",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "title_price_unique",columnNames = {"title_x","price"})
+        },
+        indexes = {
+                @Index(name = "sku_index",columnList = "sku")
+        }
+
+)
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,length = 20)
+    private String sku;
+
+    @Column(name="title_x")
+    private String title;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO test.product_table(price,quantity,sku,title_x)
+VALUES
+(12.4,4,'parle123','Parle Biscuit'),
+(10.3,2,'lays121','Lays'),
+(13.0,3,'chocolate134','Dairy Milk'),
+(14.2,12,'milk111','Milk');
