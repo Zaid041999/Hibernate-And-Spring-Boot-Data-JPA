9352109145806799f479d9d576a2487897a0b056
Zaid <106821960+Zaid041999@users.noreply.github.com>
2024-08-14 11:59:18 +0530

    Delete springbootwebtutorial directory

--- springbootwebtutorial/src/main/java/com/codingshuttle/week2/springbootwebtutorial/springbootwebtutorial/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.codingshuttle.week2.springbootwebtutorial.springbootwebtutorial.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
